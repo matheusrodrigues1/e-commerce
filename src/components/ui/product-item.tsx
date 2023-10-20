9fc4f685344da4504c3dab7f57ca19cbc1212101
matheusrodrigues1 <matech00@outlook.com>
2023-10-20 14:27:01 -0300

    feat: add Productitem and ProductList components

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -1,14 +1,16 @@
-import { Product } from "@prisma/client";
+import { ProductWithTotalPrice } from "@/helpers/product";
 import Image from "next/image";
+import { Badge } from "./badge";
+import { ArrowDown, ArrowDown01Icon } from "lucide-react";
 
 interface ProductItemProps {
-  product: Product;
+  product: ProductWithTotalPrice;
 }
 
 const ProductItem = ({product}: ProductItemProps) => {
   return (
     <div className="flex flex-col gap-4 max-w-[156px]">
-      <div className="bg-accent rounded-lg h-[170px] w-[156px] flex items-center justify-center">
+      <div className="relative bg-accent rounded-lg h-[170px] w-[156px] flex items-center justify-center">
         <Image
           src={product.imageUrls[0]}
           width={0}
@@ -20,9 +22,28 @@ const ProductItem = ({product}: ProductItemProps) => {
             objectFit: 'contain'
           }}
         />
+        {product.discountPercentage > 0 && (
+          <Badge className="absolute left-3 top-3 px-2 py-[2px]">
+            <ArrowDown size={14}/> {product.discountPercentage}%
+          </Badge>
+        )}
       </div>
-      <div>
+
+      <div className="flex flex-col gap-1">
         <p className="text-sm overflow-hidden whitespace-nowrap text-ellipsis">{product.name}</p>
+      </div>
+      <div className="flex item-center gap-2">
+        {product.discountPercentage > 0 ? (
+          <>
+            <p className="font-semibold">R$ {product.totalPrice.toFixed(2)}</p>
+          
+            <p className="opacity-75 line-through text-xs">R$ {Number(product.basePrice).toFixed(2)}</p>
+          </>
+        ) : (
+          <p className="opacity-75 line-through text-xs">R$ {Number(product.basePrice).toFixed(2)}</p>
+        )}
+
+        {}
       </div>
     </div>
    );
