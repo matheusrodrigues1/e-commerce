24c9ba953401b9cdf24bc6c50a8c382ebcd08a7a
matheusrodrigues1 <matech00@outlook.com>
2023-10-20 14:53:11 -0300

    home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -22,13 +22,24 @@ export default async function Home() {
         sizes='100vw'
         alt="banner 55% de desconto"
       />
+
       <div className='mt-8 px-5'>
         <Categories/>
       </div>
 
       <div className='mt-8'>
+        <p className='font-bold uppercase pl-5 mb-3'>Ofertas</p>
         <ProductList products={deals}/>
       </div>
+
+      <Image
+        src="/banner-home-02.png"
+        height={150}
+        width={350}
+        className='h-auto w-full px-5'
+        sizes='100vw'
+        alt="banner 55% de desconto em mouses"
+      />
     </div>
   )
 
