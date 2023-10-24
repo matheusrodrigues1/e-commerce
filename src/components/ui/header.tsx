a36a373c8128d9567ad256a8f7be4aa107f0af99
matheusrodrigues1 <matech00@outlook.com>
2023-10-24 12:13:51 -0300

    feat: ensure user goes to home page on catalog

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -88,7 +88,9 @@ const Header = () =>  {
           </div>
         </SheetContent>
       </Sheet>
-      <h1 className='text-lg font-semibold'><span className='text-primary'>FSW</span> Store</h1>
+      <Link href='/'>
+        <h1 className='text-lg font-semibold'><span className='text-primary'>FSW</span> Store</h1>
+      </Link>
       <Button size="icon" variant="outline">
         <ShoppingCartIcon/>
       </Button>
