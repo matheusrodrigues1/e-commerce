b99194af5bb2bf5eacb1787dacbdff67e4a0679f
matheusrodrigues1 <matech00@outlook.com>
2023-10-26 13:46:20 -0300

    feat: add productimage components

---
+++ src/app/product/[slug]/components/product-images.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import Image from "next/image";
+import { useState } from "react";
+
+interface ProductImagesProps {
+  name: string;
+  imageUrls: string[];
+}
+
+const ProductImages = ({imageUrls, name}: ProductImagesProps) => {
+  const [currentImage, setCurrentImage] = useState(imageUrls[0]);
+
+  const handleImageClick = (imageUrl: string) => {
+    setCurrentImage(imageUrl)
+  }
+  return (
+    <div className="flex flex-col">
+      <div className="flex h-[380px] w-full items-center justify-center bg-accent">
+        <Image src={currentImage} alt={name} height={0} width={0} sizes="100vw"
+          className="h-auto max-h-[70%] w-auto max-w-[80%] object-contain"
+        />
+      </div>
+
+      <div className="grid grid-cols-4 gap-4 mt-8 px-5">
+        {imageUrls.map((imageUrl) => (
+          <button key={imageUrl} className={`bg-accent h-[100px] rounded-lg flex justify-center items-center
+              ${imageUrl === currentImage && "border-2 border-solid border-primary"}`
+            }
+            onClick={() => handleImageClick(imageUrl)}
+          >
+            <Image src={imageUrl}
+              alt={name}
+              height={0}
+              width={0}
+              sizes="100vw"
+              className="h-auto max-h-[70%] w-auto max-w-[80%]"
+            />
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
+ 
+export default ProductImages;
--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { prismaClient } from "@/lib/prisma";
+import ProductImages from "./components/product-images";
 
 interface ProductDetailsPageProps {
   params: {
@@ -17,7 +18,9 @@ const ProductDetailsPage  = async ({params: {slug}}: ProductDetailsPageProps) =>
     return null;
   }
   return (
-    <h1>{product.name}</h1>
+    <div>
+      <ProductImages imageUrls={product.imageUrls} name={product.name} />
+    </div>
    );
 }
  
